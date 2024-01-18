c317465d654134eab152e31d2061fa8badb5a10d
LingdenHem <hemcoolgamer1@gmail.com>
2024-01-18 14:38:04 +0100

    changed space token

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,24 +6,31 @@ module.exports = {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
     navLinks: [
-      {link: "/", name: "Home"},
-      {link: "/project", name: "Projects"},
-      {link: "/about", name: "About"},
-      {link: "/contact", name: "Contact"},
-    ]
+      { link: "/", name: "Home" },
+      { link: "/project", name: "Projects" },
+      { link: "/about", name: "About" },
+      { link: "/contact", name: "Contact" },
+    ],
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      accessToken: "Tcsln1XNTqRVzcxbXYBkqhr_DyoT9vAMnFhBQvOV8iE",
-      spaceId: "75ijk98hye8q" // Replace with your Contentful Space ID
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      name: "images",
-      path: "./src/images/"
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.SPACE_ID,
+        accessToken: process.env.ACCESS_TOKEN,
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
 };
